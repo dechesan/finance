115ecd4556f68c82cdf5905b862b8592e81f0301
dechesan <deche93@outlook.com>
2024-04-28 17:24:06 -0600

    Minor formatting changes suggested by style50

--- app.py
+++ app.py
@@ -30,8 +30,10 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 @app.route("/")
 @login_required
@@ -64,35 +66,36 @@ def buy():
         symbol = request.form.get("symbol")
         if not symbol:
             return apology("missing symbol")
-        
+
         # Look up symbol on Yahoo Finance using helper function
         share = lookup(symbol)
 
         # Make sure symbol is valid
         if not share:
             return apology("invalid symbol")
-        
+
         # Make sure user entered the number of shares they wish to buy
         shares_buying = request.form.get("shares")
         if not shares_buying:
             return apology("missing shares")
-        
+
         # Make sure user entered an integer
         if not shares_buying.isdigit():
             return apology("enter a whole number")
 
         # Change from string to int
         shares_buying = int(shares_buying)
-        
+
         # Make sure user entered positive number
         if shares_buying < 1:
             return apology("enter positive number")
-        
+
         # Get share price
         price = share["price"]
 
         # Get user's current cash
-        user_cash_before = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
+        user_cash_before = db.execute(
+            "SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
 
         # The amount of money the user needs in order to complete the transaction
         cash_needed = price * shares_buying
@@ -118,7 +121,7 @@ def buy():
 
         # Return user to their portfolio page
         return redirect("/")
-    
+
     # User reached route via GET
     else:
         return render_template("buy.html")
@@ -129,7 +132,8 @@ def buy():
 def history():
     """Show history of transactions"""
     # Get all transactions for the user from the DB
-    transactions = db.execute("SELECT symbol, shares, price, datetime FROM transactions WHERE user_id = ?", session["user_id"])
+    transactions = db.execute(
+        "SELECT symbol, shares, price, datetime FROM transactions WHERE user_id = ?", session["user_id"])
 
     # Render the results
     return render_template("history.html", transactions=transactions)
@@ -197,14 +201,14 @@ def quote():
         # Make sure user didn't submit an empty form
         if not symbol:
             return apology("missing symbol")
-        
+
         # Look up the symbol on Yahoo finance using helper function
         share = lookup(symbol)
 
         # Make sure symbol is valid
         if not share:
             return apology("invalid symbol")
-        
+
         # Display the quote
         return render_template("quoted.html", symbol=share["symbol"], price=usd(share["price"]))
 
@@ -222,28 +226,29 @@ def register():
         # Make sure username is not blank
         if not request.form.get("username"):
             return apology("must provide username")
-        
+
         # Make sure username doesn't already exist
         if len(db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))) != 0:
             return apology("username already exists")
-        
+
         # Make sure neither the password or the confirmation are blank
         if not request.form.get("password") or not request.form.get("confirmation"):
             return apology("must enter password twice")
-        
+
         # Make sure passwords match
         if request.form.get("password") != request.form.get("confirmation"):
             return apology("must enter matching passwords")
-        
+
         # Generate hash of password to avoid saving the actual password directly in the db
         password_hash = generate_password_hash(request.form.get("password"))
 
         # Add the user to the db
-        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), password_hash)
-        
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
+                   request.form.get("username"), password_hash)
+
         # Return user to homepage
         return redirect("/")
-    
+
     # User reached route via GET
     else:
         return render_template("register.html")
@@ -263,34 +268,34 @@ def sell():
     # Populate list of symbols owned by the user
     for row in portfolio:
         symbols.append(row['symbol'])
-    
+
     # User reached route via POST (as by submitting the sell form)
     if request.method == "POST":
         # Get the symbol entered by the user of the stock they wish to sell
         symbol = request.form.get("symbol")
-        
+
         # Make sure the user actually entered a symbol
         if not symbol:
             return apology("missing symbol")
-        
+
         # Make sure the user entered a symbol for a stock they own
         if symbol not in symbols:
             return apology("invalid symbol")
-        
+
         # Get the amount of shares entered by the user
         shares_selling = request.form.get("shares")
 
         # Make sure there is input for share amount
         if not shares_selling:
             return apology("missing shares")
-        
+
         # Make sure user entered an integer
         if not shares_selling.isdigit():
             return apology("invalid 'shares' input")
-        
+
         # Convert value to int
         shares_selling = int(shares_selling)
-        
+
         # Make sure user entered a valid number of shares to sell
         if shares_selling < 1:
             return apology("shares less than 1")
@@ -301,7 +306,7 @@ def sell():
         # Initializing variable for the current price of the selected stock
         price = 0
 
-        # Get the number and price of shares held by the user for their selected stock 
+        # Get the number and price of shares held by the user for their selected stock
         for row in portfolio:
             if row['symbol'] == symbol:
                 shares_held_before = row['shares']
@@ -312,7 +317,8 @@ def sell():
             return apology("insufficient shares")
 
         # Sell the shares
-        cash_held_before = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
+        cash_held_before = db.execute(
+            "SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
         cash_gained = shares_selling * price
         cash_held_after = cash_held_before + cash_gained
 
@@ -328,7 +334,7 @@ def sell():
 
         # Redirect user to homepage
         return redirect("/")
-    
+
     # User reached route via GET
     else:
         return render_template("sell.html", symbols=symbols)
@@ -347,20 +353,21 @@ def funds():
         # Make sure the user didn't submit a blank form
         if not cash_adding:
             return apology("missing amount")
-        
+
         # Make sure user entered a number
         try:
             cash_adding = float(cash_adding)
         except ValueError:
             return apology("enter a number")
-        
+
         # Make sure the user entered at least 1 dollar
         if cash_adding < 1:
             return apology("minimum amount is $1.00")
-        
+
         # Get the user's current cash from the DB, and add the new funds to that amount
-        new_total = float(db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]) + cash_adding
-        
+        new_total = float(db.execute("SELECT cash FROM users WHERE id = ?",
+                          session["user_id"])[0]["cash"]) + cash_adding
+
         # Update the user's cash amount in the DB
         db.execute("UPDATE users SET cash = ? WHERE id = ?", new_total, session["user_id"])
 
@@ -369,19 +376,20 @@ def funds():
 
         # Redirect user to homepage
         return redirect("/")
-    
+
     # User reached route via GET
     else:
         # Display the form to add funds
         return render_template("funds.html")
 
+
 @app.route("/settings", methods=["GET", "POST"])
 @login_required
 def settings():
-     """ Show settings for the user's account """
+    """ Show settings for the user's account """
 
     # User reached route via POST (as by submitting the form to change password)
-     if request.method == "POST":
+    if request.method == "POST":
 
         # Get user's input
         old_pass = request.form.get("old_pass")
@@ -395,18 +403,18 @@ def settings():
             return apology("please enter new password")
         if not confirmation:
             return apology("please re-enter new password")
-        
+
         # Make sure user re-entered the same password
         if not new_pass == confirmation:
             return apology("passwords do not match")
-        
+
         # Get the hash of the old password from DB
         old_hash = db.execute("SELECT hash FROM users WHERE id = ?", session["user_id"])[0]["hash"]
 
         # Check if the old password entered by the user is correct
         if not check_password_hash(old_hash, old_pass):
             return apology("old password is incorrect")
-        
+
         # Make a hash for the new password
         new_hash = generate_password_hash(new_pass)
 
@@ -418,15 +426,16 @@ def settings():
 
         # Reload page
         return redirect("/settings")
-     
-     # User reached page via GET
-     else:
+
+    # User reached page via GET
+    else:
         return render_template("settings.html")
 
 
 def get_portfolio():
     # Get the symbols from all of the user's transactions. Returns list of dicts
-    symbols = db.execute("SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol", session["user_id"])
+    symbols = db.execute(
+        "SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol", session["user_id"])
 
     # Convert list of dicts into a list of all dict values using list comprehension
     symbols = [(d['symbol']) for d in symbols]
@@ -436,7 +445,8 @@ def get_portfolio():
 
     # Populate portfolio
     for symbol in symbols:
-        shares = int(db.execute("SELECT SUM(shares) FROM transactions WHERE user_id = ? AND symbol=? GROUP BY symbol", session["user_id"], symbol)[0]['SUM(shares)'])
+        shares = int(db.execute("SELECT SUM(shares) FROM transactions WHERE user_id = ? AND symbol=? GROUP BY symbol",
+                     session["user_id"], symbol)[0]['SUM(shares)'])
         if shares > 0:
             price = float(lookup(symbol)["price"])
             total = shares * price
